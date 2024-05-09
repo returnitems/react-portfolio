68cc6140cfe007802b98a9ceacb4f69d37c6db0a
diwanumang <diwan.umang@gmail.com>
2024-05-09 18:27:08 -0400

    Used variables, conditionals, and loops

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const mySkills = [
+  {
+    name: "JavaScript",
+    comfort: 9,
+    frontEnd: true,
+    backend: true,
+  },
+  {
+    name: "CSS",
+    comfort: 8,
+    frontEnd: true,
+    backend: false,
+  },
+  {
+    name: "HTML",
+    comfort: 10,
+    frontEnd: true,
+    backend: false,
+  },
+  {
+    name: "Python",
+    comfort: 3,
+    frontEnd: false,
+    backend: true,
+  },
+  {
+    name: "EJS",
+    comfort: 6,
+    frontEnd: false,
+    backend: true,
+  },
+  {
+    name: "Express",
+    comfort: 6,
+    frontEnd: false,
+    backend: true,
+  },
+];
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Welcome to my portfolio!</h1>
+    <h4>These are my current skills</h4>
+    <ul>
+      {mySkills.map((skill, index) => 
+        skill.comfort > 5 ? <li key={index}> {skill.name} </li> : null
+      )}
+    </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
